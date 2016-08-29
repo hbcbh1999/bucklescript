b034aff6bb5d0700ea56aaa4b4926b470c4651d4
Hongbo Zhang <bobzhang1988@gmail.com>
2016-08-29 17:22:19 -0400

    more docs and more tests

--- jscomp/test/typeof_test.js
+++ jscomp/test/typeof_test.js
@@ -26,7 +26,7 @@ var suites_001 = /* :: */[
   ],
   /* :: */[
     /* tuple */[
-      "int_gadt_test",
+      "number_gadt_test",
       function () {
         return /* Neq */Block.__(1, [
                   Js.test(3, /* Number */3),
@@ -34,7 +34,88 @@ var suites_001 = /* :: */[
                 ]);
       }
     ],
-    /* [] */0
+    /* :: */[
+      /* tuple */[
+        "boolean_gadt_test",
+        function () {
+          return /* Neq */Block.__(1, [
+                    Js.test(true, /* Boolean */2),
+                    Js.$$null
+                  ]);
+        }
+      ],
+      /* :: */[
+        /* tuple */[
+          "undefined_gadt_test",
+          function () {
+            return /* Neq */Block.__(1, [
+                      Js.test(Js.$$undefined, /* Undefined */0),
+                      Js.$$null
+                    ]);
+          }
+        ],
+        /* :: */[
+          /* tuple */[
+            "null_gadt_test",
+            function () {
+              return /* Neq */Block.__(1, [
+                        Js.test(Js.$$null, /* Null */1),
+                        Js.$$null
+                      ]);
+            }
+          ],
+          /* :: */[
+            /* tuple */[
+              "string_gadt_test",
+              function () {
+                return /* Neq */Block.__(1, [
+                          Js.test("3", /* String */4),
+                          Js.$$null
+                        ]);
+              }
+            ],
+            /* :: */[
+              /* tuple */[
+                "string_gadt_test_neg",
+                function () {
+                  return /* Eq */Block.__(0, [
+                            Js.test(3, /* String */4),
+                            Js.$$null
+                          ]);
+                }
+              ],
+              /* :: */[
+                /* tuple */[
+                  "function_gadt_test",
+                  function () {
+                    return /* Neq */Block.__(1, [
+                              Js.test(function (x) {
+                                    return x;
+                                  }, /* Function */5),
+                              Js.$$null
+                            ]);
+                  }
+                ],
+                /* :: */[
+                  /* tuple */[
+                    "object_gadt_test",
+                    function () {
+                      return /* Neq */Block.__(1, [
+                                Js.test({
+                                      x: 3
+                                    }, /* Object */6),
+                                Js.$$null
+                              ]);
+                    }
+                  ],
+                  /* [] */0
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ]
   ]
 ];
 
--- jscomp/test/typeof_test.ml
+++ jscomp/test/typeof_test.ml
@@ -5,7 +5,15 @@
 let suites = Mt.[
     "int_type", (fun _ -> Eq(Js.unsafe_typeof 3, "number") );
     "string_type", (fun _ -> Eq(Js.unsafe_typeof "x", "string"));
-    "int_gadt_test", (fun _ -> Neq (Js.test 3 Js.Number, Js.null ))    
+
+    "number_gadt_test", (fun _ -> Neq (Js.test 3 Js.Number, Js.null ))  ;  
+    "boolean_gadt_test", (fun _ -> Neq (Js.test Js.true_ Js.Boolean, Js.null ))    ;
+    "undefined_gadt_test", (fun _ -> Neq (Js.test Js.undefined Js.Undefined, Js.null ))    ;
+    "null_gadt_test", (fun _ -> Neq (Js.test Js.null  Js.Null, Js.null ));    
+    "string_gadt_test", (fun _ -> Neq (Js.test "3" Js.String, Js.null ));    
+    "string_gadt_test_neg", (fun _ -> Eq (Js.test 3 Js.String, Js.null ));    
+    "function_gadt_test", (fun _ -> Neq (Js.test (fun  x -> x ) Js.Function, Js.null )) ;
+    "object_gadt_test", (fun _ -> Neq (Js.test [%bs.obj{x = 3}] Js.Object, Js.null ))    
 ]
 
 ;; Mt.from_pair_suites __FILE__ suites 
